c9d0cd21cfcb62bf0e7d3ce34be31685e7fe862b
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2024-03-13 14:58:32 +0100

    Finished login

--- LoginScreen.dfm
+++ LoginScreen.dfm
@@ -10,6 +10,7 @@ object Form1: TForm1
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  OnCreate = FormCreate
   TextHeight = 15
   object Label1: TLabel
     Left = 168
@@ -25,11 +26,29 @@ object Form1: TForm1
     Height = 15
     Caption = 'Wachtwoord'
   end
+  object lblError: TLabel
+    Left = 168
+    Top = 331
+    Width = 9
+    Height = 15
+    Caption = '...'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clRed
+    Font.Height = -12
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
   object sbtnLogin: TAdvSmoothButton
     Left = 168
     Top = 272
     Width = 217
     Height = 35
+    Appearance.Font.Charset = DEFAULT_CHARSET
+    Appearance.Font.Color = clWhite
+    Appearance.Font.Height = -12
+    Appearance.Font.Name = 'Segoe UI'
+    Appearance.Font.Style = [fsBold]
     Appearance.SimpleLayout = True
     Appearance.Rounding = 10
     Status.Caption = '0'
@@ -49,8 +68,10 @@ object Form1: TForm1
     Status.Appearance.Font.Style = []
     Caption = 'Login'
     Color = 3023058
+    ParentFont = False
     TabOrder = 0
     Version = '2.2.3.1'
+    OnClick = sbtnLoginClick
     TMSStyle = 8
   end
   object edtUser: TEdit
@@ -67,5 +88,20 @@ object Form1: TForm1
     Height = 23
     PasswordChar = #8226
     TabOrder = 2
+  end
+  object pgcDBconnection: TPgConnection
+    Username = 'stage2'
+    Server = '142.132.213.151'
+    LoginPrompt = False
+    Database = 'rchat'
+    Connected = True
+    Left = 88
+    Top = 56
+    EncryptedPassword = 'CDFFACFF8BFF9EFF98FF9AFFD5FF'
+  end
+  object pgqGetUser: TPgQuery
+    Connection = pgcDBconnection
+    Left = 208
+    Top = 56
   end
 end
--- LoginScreen.pas
+++ LoginScreen.pas
@@ -4,7 +4,9 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, AdvSmoothButton;
+  StrUtils,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, AdvSmoothButton, Data.DB,
+  DBAccess, PgAccess, MemDS;
 
 type
   TForm1 = class(TForm)
@@ -13,6 +15,11 @@ type
     edtPassword: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    pgcDBconnection: TPgConnection;
+    pgqGetUser: TPgQuery;
+    lblError: TLabel;
+    procedure sbtnLoginClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,5 +32,51 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  lblError.Caption := '';
+end;
+
+procedure TForm1.sbtnLoginClick(Sender: TObject);
+begin
+  pgcDBconnection.Open;
+
+  if((Length(edtUser.Text) > 0) AND (Length(edtPassword.Text) > 0)) then
+  begin
+
+    pgqGetUser.SQL.Text := '';
+    pgqGetUser.SQL.Add('SELECT * FROM tbl_gebruikers');
+
+    if(ContainsText(edtUser.Text, '@')) then
+    begin
+      pgqGetUser.SQL.Add('WHERE LOWER(gbr_email)=:user');
+    end
+    else
+    begin
+      pgqGetUser.SQL.Add('WHERE LOWER(gbr_nicknaam)=:user');
+    end;
+
+    pgqGetUser.SQL.Add('AND gbr_wachtwoord=:password');
+    pgqGetUser.ParamByName('user').AsString := Trim(LowerCase(edtUser.Text));
+    pgqGetUser.ParamByName('password').AsString := edtPassword.Text;
+    pgqGetUser.Open;
+
+    if(pgqGetUser.RecordCount > 0) then
+    begin
+      ShowMessage('Succes');
+    end
+    else
+    begin
+      lblError.Caption := 'Username of wachtwoord is incorrect';
+    end;
+
+  end
+  else
+  begin
+    lblError.Caption := 'Gebruikersnaam of wachtwoord is leeg';
+  end;
+
+end;
 
 end.
